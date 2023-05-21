b6244309063b32243f688d2b275521e4be69277c
shanif <shanif@example.com>
2023-05-21 16:50:11 +0500

    alpha-blog on message validation

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -3,4 +3,23 @@ class ArticlesController < ApplicationController
         # debugger
         @article = Article.find(params[:id]) # params --> means articles/1 <--parameters
     end
+
+    def index
+        @articles = Article.all
+    end
+
+    def new
+
+    end
+
+    def create
+        #  render plain: params[:article]
+        @article = Article.new(params.require(:article).permit(:title, :description))
+        if @article.save
+            # redirect_to article_path(@article) #shortcut is -->
+            redirect_to @article
+        else
+            render :new
+        end
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,5 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   root "pages#home"
-  resources :articles, only: [:show]
+  resources :articles, only: [:show, :index, :new, :create]
 end
