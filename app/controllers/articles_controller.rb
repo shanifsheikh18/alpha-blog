60f001179a5c320b8ecb34ebe5d7798466cd693f
shanif <shanif@example.com>
2023-05-22 13:43:31 +0500

    alpha-blog section 4 done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,8 @@
 class ArticlesController < ApplicationController
+    before_action :set_article, only: [ :show, :edit, :update, :destroy ]
+
     def show
         # debugger
-        @article = Article.find(params[:id]) # params --> means articles/1 <--parameters
     end
 
     def index
@@ -9,17 +10,47 @@ class ArticlesController < ApplicationController
     end
 
     def new
+        @article = Article.new
+    end
+
+    def edit
 
     end
 
     def create
         #  render plain: params[:article]
-        @article = Article.new(params.require(:article).permit(:title, :description))
+        @article = Article.new(article_params)
         if @article.save
+            flash[:notice] = "Article was created successfully."
             # redirect_to article_path(@article) #shortcut is -->
             redirect_to @article
         else
-            render :new
+            render 'new', status: :unprocessable_entity
         end
+    end
+
+    def update
+        if @article.update(article_params)
+            flash[:notice] = "Article was updated successfully."
+            redirect_to @article
+        else
+            render :edit, status: :unprocessable_entity
+        end
+    end
+
+    def destroy
+        # link_to does not work for destroy method, instead you have to use button_to 
+        @article.destroy
+        redirect_to articles_path
+    end
+
+    private
+
+    def set_article
+        @article = Article.find(params[:id])
+    end
+
+    def article_params
+        params.require(:article).permit(:title, :description)
     end
 end
